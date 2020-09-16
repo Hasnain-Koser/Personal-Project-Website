e1949e154c70421f49a66390bd202fc82a01813a
Hasnain-Koser <hasnain.koser@gmail.com>
2020-09-16 09:56:40 +0530

    hi

---
+++ about.js
@@ -0,0 +1,30 @@
+
+function who_open() {
+    document.getElementById("who").style.width = "60%";
+    setTimeout(function() {
+        document.getElementById
+        document.getElementById("who").style.color = "white";
+    }, 1000)
+}
+setTimeout(who_open, 1000)
+function what_open() {
+    document.getElementById("what").style.width = "60%";
+    setTimeout(function() {
+    document.getElementById("what").style.color = "white";
+    }, 1000)
+}
+setTimeout(what_open, 1500)
+function why_open() {
+    document.getElementById("why").style.width = "60%";
+    setTimeout(function() {
+    document.getElementById("why").style.color = "white";
+    }, 1000)
+}
+setTimeout(why_open, 2000)
+function how_open() {
+    document.getElementById("how").style.width = "60%";
+    setTimeout(function() {
+    document.getElementById("how").style.color = "white";
+    }, 1000)
+}
+setTimeout(how_open, 2500)
